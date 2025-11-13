cac1ceb99600c2a968ffd8f36f0d395fbf6953a3
Daniel Ferreira <silva_120000@yahoo.com.br>
2025-11-13 06:08:03 -0300

    criacao do componente de contagem e o form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Container } from './components/Container';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import './styles/global.css';
@@ -13,6 +15,30 @@ function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
