c5dd197dfbfa5f3947c239ffebd10d9458cfc97b
Daniel Ferreira <silva_120000@yahoo.com.br>
2025-11-16 10:05:27 -0300

    criando models de task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
+import { TaskContext } from './contexts/TaskContext';
 
-function App() {
-  return <Home />;
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
+export function App() {
+  const [state, setState] = useState(initialState);
+  return (
+    <TaskContext.Provider value={initialState}>
+      <Home />;
+    </TaskContext.Provider>
+  );
 }
-
-export default App;
