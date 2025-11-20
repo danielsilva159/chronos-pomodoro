554dfd8ccd07947ac322b3c9e77333fa811e286a
Daniel Ferreira <silva_120000@yahoo.com.br>
2025-11-20 06:27:08 -0300

    criando os ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,11 +1,53 @@
 import { PlayCircleIcon } from 'lucide-react';
+import { useRef } from 'react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    if (taskNameInput.current === null) return;
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptdate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
   return (
     <form onSubmit={handleCreateNewTask} className='form'>
@@ -15,16 +57,19 @@ export function MainForm() {
           id='meuInput'
           type='text'
           labelText='task'
+          ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
       <div className='formRow'>
         <p>Lorem ipsum dolor sit amet.</p>
       </div>
-
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
       </div>
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 
@@ -9,6 +9,9 @@ type TaskContextProviderProps = {
 export function TaskContextProvide({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
+  useEffect(() => {
+    console.log('state', state);
+  }, [state]);
   return (
     <TaskContext.Provider value={{ state, setState }}>
       {children}
